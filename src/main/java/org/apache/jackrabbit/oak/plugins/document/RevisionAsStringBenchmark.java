2e3baa8633153642023acca93e6d80da230870b9
Chetan Mehrotra <chetanm@adobe.com>
2016-06-02 17:09:47 +0530

    Remove threading

--- src/main/java/org/apache/jackrabbit/oak/plugins/document/RevisionAsStringBenchmark.java
+++ src/main/java/org/apache/jackrabbit/oak/plugins/document/RevisionAsStringBenchmark.java
@@ -28,17 +28,6 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class RevisionAsStringBenchmark {
-    /**
-     * All possible chars for representing a number as a String
-     */
-    final static char[] digits = {
-            '0' , '1' , '2' , '3' , '4' , '5' ,
-            '6' , '7' , '8' , '9' , 'a' , 'b' ,
-            'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
-            'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
-            'o' , 'p' , 'q' , 'r' , 's' , 't' ,
-            'u' , 'v' , 'w' , 'x' , 'y' , 'z'
-    };
 
     @State(Scope.Benchmark)
     public static class BenchmarkState {
@@ -141,8 +130,7 @@ public class RevisionAsStringBenchmark {
         Options opt = new OptionsBuilder()
                 .include(RevisionAsStringBenchmark.class.getName() + ".*")
                 .warmupIterations(5)
-                .measurementIterations(5)
-                .threads(4)
+                .measurementIterations(10)
                 .forks(1)
                 .shouldFailOnError(true)
                 .shouldDoGC(true)
